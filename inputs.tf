b604989794a419d6bc483dbd3e7b5bac282d9d70
manu <gatlamanohar199@gmail.com>
2023-04-11 14:56:00 +0530

    added changes

--- inputs.tf
+++ inputs.tf
@@ -15,6 +15,6 @@ variable "lb_vpc_info" {
     lb_subnet_names      = ["web", "app"]
     lb_subnets_names_azs = ["a", "b"]
     lb_vpc_cidr          = "192.168.0.0/16"
-    rollout_versions     = "0.0.0.1"
+    rollout_versions     = "0.0.0.8"
   }
 }
