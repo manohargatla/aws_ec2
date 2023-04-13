047714acdf450ba0c9bd5ca314a9d9ce390bf866
manu <gatlamanohar199@gmail.com>
2023-04-13 12:26:42 +0530

    added changes

--- inputs.tf
+++ inputs.tf
@@ -6,17 +6,15 @@ variable "region" {
 ## variable for vpc cidr
 variable "lb_vpc_info" {
   type = object({
-    ec2_names = list(string)
     lb_vpc_cidr          = string
     lb_subnet_names      = list(string)
     lb_subnets_names_azs = list(string)
     rollout_versions     = string
   })
   default = {
-    ec2_names = ["red", "green"]
     lb_subnet_names      = ["web", "app"]
     lb_subnets_names_azs = ["a", "b"]
     lb_vpc_cidr          = "192.168.0.0/16"
-    rollout_versions     = "0.0.0.8"
+    rollout_versions     = "0.0.0.13"
   }
 }
--- resources.tf
+++ resources.tf
@@ -1,13 +1,11 @@
-
 ## create keypair
 resource "aws_key_pair" "deployer" {
   key_name   = "terraform3"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCyNRL9nyxUnjeqSr92yVqV4ImkfwR6qYQrBBR5+eaxrCDQhIoHUtgiG0YXjrhXl6E6ErKiZBgwGjjFsMqjdzsfS9kHiawTMxTr4ilwCfOChgDfR5t5e3L/X4F/ZjCZiK1qNha+/DC5r/dGwhB579yxXSUxVWfGOP4buGWkWBWpmrN94EMmtFdyBSjnjMardSV2mXXPjPDNDudDUMEsQr4P8aAbiOj9VCf2tpQswElkjA4IZ8DfIfeIwKYsR11uDAqZrSf96TxFXN6OCKOnqu4DSWxFbKywffS5XG+nTC1+oee/ftdL6rlJpg/VaTN4Bqfsk9px/redvXlNFUsaZqrm5UiLCS7QGO/HfPa57JQBsS+jv2fURQfYMg35otxtbE3+IIHLzmdNnQOVU/scTyuO73kHrU2w0zTqfbMbqm7CqpnBfrdyzI4+AnV/4HtYojxGTZR6S3oV0azc7eKAGyeUjMttTuVbDYlQInkvZvS4SrFSfRTk+v1CFX0IJvSlVFE= dell@DESKTOP-G8OJBDS"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 ## create EC2 instance
 resource "aws_instance" "red" {
-  count = length(lb_vpc_info.ec2_names) == true ? red : green
   instance_type               = "t2.micro"
   associate_public_ip_address = "true"
   ami                         = "ami-007855ac798b5175e"
@@ -52,3 +50,48 @@ resource "null_resource" "spc" {
   }
 }
 
+## create EC2 instance
+resource "aws_instance" "green" {
+  instance_type               = "t2.micro"
+  associate_public_ip_address = "true"
+  ami                         = "ami-007855ac798b5175e"
+  subnet_id                   = aws_subnet.lb_subnet[1].id
+  vpc_security_group_ids      = [aws_security_group.terraformlb.id]
+  key_name                    = "terraform3"
+  tags = {
+    Name = "green"
+  }
+  depends_on = [
+    aws_security_group.terraformlb
+  ]
+}
+
+## create null resoure
+resource "null_resource" "spc1" {
+  triggers = {
+    rollout_versions = var.lb_vpc_info.rollout_versions
+  }
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host        = aws_instance.green.public_ip
+  }
+  provisioner "file" {
+    source      = "./spc.service"
+    destination = "/tmp/spc.service"
+
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install openjdk-17-jdk maven -y",
+      "sudo cp /tmp/spc.service /etc/systemd/system/spc.service",
+      "git clone https://github.com/spring-projects/spring-petclinic.git",
+      "cd spring-petclinic",
+      "./mvnw package",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl start spc.service"
+    ]
+  }
+}
